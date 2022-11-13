3a14d40194021abd19671bed4dff43b13bffedec
developer-sahadat <mdridoysikder862@gmail.com>
2022-11-13 19:26:37 +0600

    loading updated

--- src/Components/Pages/NewsPages.js
+++ src/Components/Pages/NewsPages.js
@@ -3,55 +3,53 @@ import Loading from "../Shear/Loading";
 import UseNews from "../../Hook/UseNews";
 const NewsPages = () => {
   const [news, newsLoading] = UseNews();
-  console.log(news);
+
   return (
     <>
-      {newsLoading ? (
-        <Loading />
-      ) : (
-        <>
-          <section className="news-header">
-            <div className="container">
-              <h1>Latest News</h1>
-            </div>
-          </section>
+      <section className="news-header">
+        <div className="container">
+          <h1>Latest News</h1>
+        </div>
+      </section>
 
-          <section className="news-section">
-            <div className="container">
-              <h2>News</h2>
+      <section className="news-section">
+        <div className="container">
+          <h2>News</h2>
 
-              <div className="news">
-                <div className="row">
-                  {news?.map((data) => (
-                    <div key={data?.id} className="col">
-                      <div className="news-content">
-                        <div className="inner">
-                          <div className="thumbnail">
-                            <a href="#">
-                              <img src={data?.fimg_url} alt="thumbnail" />
-                            </a>
-                          </div>
-                          <div className="content">
-                            <h4 className="title">
-                              <a href="#">{data?.title?.rendered}</a>
-                            </h4>
-                            <p className="desc">{data?.excerpt?.rendered}</p>
-                            <a href={data?.link} className="read-btn">
-                              Read More <i className="fas fa-caret-right"></i>
-                            </a>
+          <div className="news">
+            {newsLoading ? (
+              <Loading />
+            ) : (
+              <div className="row">
+                {news?.map((data) => (
+                  <div key={data?.id} className="col">
+                    <div className="news-content">
+                      <div className="inner">
+                        <div className="thumbnail">
+                          <a href="#">
+                            <img src={data?.fimg_url} alt="thumbnail" />
+                          </a>
+                        </div>
+                        <div className="content">
+                          <h4 className="title">
+                            <a href="#">{data?.title?.rendered}</a>
+                          </h4>
+                          <p className="desc">{data?.excerpt?.rendered}</p>
+                          <a href={data?.link} className="read-btn">
+                            Read More <i className="fas fa-caret-right"></i>
+                          </a>
 
-                            <p className="date-published">{data?.date}</p>
-                          </div>
+                          <p className="date-published">{data?.date}</p>
                         </div>
                       </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          </section>
-        </>
-      )}
+            )}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
